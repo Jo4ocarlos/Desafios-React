24ddf5f77ca9a4417784f0a343f6d40ea590aa78
João Carlos de Almeida <juke4892@gmail.com>
2026-03-12 12:16:08 -0300

    avançando em props

--- Desafio_2/aprendendo_props/src/App.jsx
+++ Desafio_2/aprendendo_props/src/App.jsx
@@ -1,10 +1,18 @@
 import Cracha from "./components/Cracha";
 import ListaClientes from "./components/ListaClientes";
 import Projeto from "./components/Projeto";
+import ProdutoCard from "./components/ProdutoCard";
 const App = () => {
+  // objeto para ser usado no componente "ListaClientes"
   const meusClientes = [
     { id: 1, nome: "Carol", servico: "Gestão de Redes Sociais" },
     { id: 2, nome: "Rogerio", servico: "Manutenção WooCommerce" },
+  ];
+  //objeto para ser usado no componente "ProdutoCard"
+  const ProductsList = [
+    { id: 3, nome: "toner", preco: 10, quantidadeEstoque: 40 },
+    { id: 4, nome: "Placa ps", preco: 40, quantidadeEstoque: 0 },
+    { id: 5, nome: "Lona", preco: 800, quantidadeEstoque: 5 },
   ];
 
   return (
@@ -27,6 +35,17 @@ const App = () => {
         <h2>Meus Projetos</h2>
         <Projeto titulo="Automação n8n" concluido={true} />
         <Projeto titulo="Criação de Landing Page" concluido={false} />
+      </div>
+
+      <div className="products_area">
+        {ProductsList.map((product) => (
+          <ProdutoCard
+            key={product.id}
+            nome={product.nome}
+            preco={product.preco}
+            estoque={product.quantidadeEstoque}
+          />
+        ))}
       </div>
     </div>
   );
---
+++ Desafio_2/aprendendo_props/src/components/ProdutoCard.jsx
@@ -0,0 +1,19 @@
+import "./ProdutoCard.css"
+const ProdutoCard = ({nome, preco, estoque})=>{
+
+    return(
+        <div>
+            <div className="card_produtos">
+            <h3>{nome}</h3>
+            <span>
+                <p className= {estoque > 0 ? "": "red_text"}>{estoque > 0 ? `R$ ${preco}`: "PRODUTO ESGOTADO" }
+                </p>
+            {estoque && <button>Comprar</button>}
+            </span>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ProdutoCard;
