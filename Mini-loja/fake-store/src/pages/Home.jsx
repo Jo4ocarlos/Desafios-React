1f5a23d2f00e11f222b9748d64f93c76130241e5
João Carlos de Almeida <juke4892@gmail.com>
2026-04-16 17:54:48 -0300

    inicio da aplicação de um css básico na pagina

--- Mini-loja/fake-store/src/pages/Home.jsx
+++ Mini-loja/fake-store/src/pages/Home.jsx
@@ -1,30 +1,34 @@
 import { useFetch } from "../hooks/useFetch";
 import { Link } from "react-router-dom";
+
+//css
+import "./Home.css";
+
 const Home = () => {
   const url = "https://fakestoreapi.com/products";
 
-  const { data:products, loading, error } = useFetch(url);
+  const { data: products, loading, error } = useFetch(url);
 
   return (
     <div>
-      {loading &&  <p>Carregando seus dados...</p>}
-      {error &&  <p>{error}</p>}
-      <ul>
+      {loading && <p>Carregando seus dados...</p>}
+      {error && <p>{error}</p>}
+      <ul className="products-area">
         {products &&
           products.map((product) => (
-            
             <li key={product.id}>
-              <div>
-                <img
-                  src={product.image}
-                  alt={`imagem do produto ${product.title}`}
-                />
-              </div>
-              <p>{product.title}</p>
-              <div>
-                <p>$ {product.price}</p>
-                <Link to={`/product/${product.id}/info`}>Ver detalhes</Link>
-              </div>
+              <Link to={`/product/${product.id}/info`}>
+                <div className="product-image">
+                  <img
+                    src={product.image}
+                    alt={`imagem do produto ${product.title}`}
+                  />
+                </div>
+                <p>{product.title}</p>
+                <div className="price-info">
+                  <p>A partir de: $ {product.price}</p>
+                </div>
+              </Link>
             </li>
           ))}
       </ul>
