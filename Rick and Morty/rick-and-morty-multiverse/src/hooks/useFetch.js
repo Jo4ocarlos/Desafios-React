dadb6f303452c5be3c3b03cdd9d633f53d42c17d
João Carlos de Almeida <juke4892@gmail.com>
2026-04-20 14:22:34 -0300

    correção da paginação

--- Rick and Morty/rick-and-morty-multiverse/src/hooks/useFetch.js
+++ Rick and Morty/rick-and-morty-multiverse/src/hooks/useFetch.js
@@ -2,25 +2,32 @@ import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
   const [loading, setLoading] = useState();
-  const [error, setRrror] = useState();
+  const [error, setError] = useState();
   const [data, setData] = useState();
 
   useEffect(() => {
+    const fetchData = async () => {
+     
+      setLoading(true);
+      setData(null)
+      setError(null)
 
-    const fetchData = async ()=>{
-        try {
-            const res = await fetch(url)
-       
-            const json = await res.json()
-            setData(json.results)
-
-        } catch (error) {
-            
+      try {
+        const res = await fetch(url);
+        // Tratamento de erro HTTP (ex: 404)
+        if (!res.ok) {
+          throw new Error(`Erro HTTP: ${response.status}`);
         }
-        
-    }
+        const json = await res.json();
+        setData(json.results);
 
-    fetchData()
+      } catch (error) {
+        setError(`Ops, deu um erro na busca: ${error.message}`);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
   }, [url]);
-  return{data, loading, error}
+  return { data, loading, error };
 };
--- Rick and Morty/rick-and-morty-multiverse/src/pages/Home.jsx
+++ Rick and Morty/rick-and-morty-multiverse/src/pages/Home.jsx
@@ -1,59 +1,68 @@
 import { useState } from "react";
 import { useFetch } from "../hooks/useFetch";
-import './Home.css'
+import "./Home.css";
 const Home = () => {
-  const [pages, setPages] = useState(1)
+  const [pages, setPages] = useState(1);
   const url = `https://rickandmortyapi.com/api/character/?page=${pages}`;
   const { data: characters, loading, error } = useFetch(url);
 
-  const changePages= (e)=>{
-    console.log(e.target.value)
-    if(e.target.value === "proximo"){
-
-      setPages((prevPage)=>{
-      return prevPage + 1
-    })
-
+  const changePages = (e) => {
+    if (e.target.value === "proximo") {
+      setPages((prevPage) => {
+        return prevPage + 1;
+      });
     }
-    if(e.target.value === "anterior"){
-
-      setPages((prevPage)=>{
+    if (e.target.value === "anterior" && pages > 1) {
+      setPages((prevPage) => {
+          return prevPage - 1;
         
-        if(prevPage >= 1){
-          return prevPage - 1
-        }else{
-          return prevPage
-        }
-      
-    })
-
+      });
     }
-  
-    
-  }
+   
+  };
 
-  return <div>
-      
-    <ul>
-      {characters &&
-      characters.map((character)=>(
-        <li key={character.id}>
-          <div className="character-image">
-            <img src={character.image} alt={`foto do personagem ${character.name}`} />
-          </div>
-          <div>
-            <p>{character.name}</p>
-          </div>
-          </li>
-      ))
-      }
-    </ul>
-  <div>
-      <button onClick={changePages} value={'anterior'}>Anterior</button>
-      <p>{pages}</p>
-      <button onClick={changePages} value={'proximo'}>Próximo</button>
+  return (
+    <div>
+      {loading && <p>carregando...</p>}
+        {error && <p>{error}</p>}
+      <ul className="character-area">
+        
+        {characters &&
+          characters.map((character) => (
+            <li key={character.id} className="character-card">
+              <div>
+                <p>{character.name}</p>
+              </div>
+              <div className="character-image">
+                <img
+                  src={character.image}
+                  alt={`foto do personagem ${character.name}`}
+                />
+              </div>
+              <ul className="character-details">
+                <li>Species: {character.species}</li>
+                <li>Gender: {character.gender}</li>
+                <li>Status: {character.status}</li>
+                <li>Origin: {character.origin["name"]}</li>
+              </ul>
+            </li>
+          ))}
+      </ul>
+      <div>
+        <button
+          onClick={changePages}
+          value={"anterior"}
+          disabled={pages === 1 || loading}
+        >
+          Anterior
+        </button>
+        <p>{pages}</p>
+        <button onClick={changePages} value={"proximo"}>
+          Próximo
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Home;
