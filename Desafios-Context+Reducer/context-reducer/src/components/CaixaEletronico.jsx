54621ef4d14287d3297ce4a9fea36fac81999357
João Carlos de Almeida <juke4892@gmail.com>
2026-04-24 15:49:58 -0300

    resolução do desafio caixa eletrônico

--- Desafios-Context+Reducer/context-reducer/src/components/CaixaEletronico.jsx
+++ Desafios-Context+Reducer/context-reducer/src/components/CaixaEletronico.jsx
@@ -2,22 +2,26 @@ import { useState } from "react";
 import { useBankContext } from "../hooks/useBankContext";
 
 const CaixaEletronico = () => {
-  const [credito, setCredito] = useState();
-  const { saldo, dispach } = useBankContext();
+  const [credito, setCredito] = useState(0);
+  const { saldo, dispatch } = useBankContext();
 
   const depositarSaldo = (value) => {
-    const newValue = Number(value + saldo);
-    dispach({ type: "DEPOSITAR", dados: newValue });
+    
+    dispatch({ type: "DEPOSITAR", dados:value});
+    setCredito('')
   };
+
   const sacarSaldo = (value) => {
-    const newValue = Number(value - saldo);
-    dispach({ type: "SACAR", dados: newValue });
+    dispatch({ type: "SACAR", dados:value});
+    setCredito('')
   };
 
   return (
     <div>
       <h3>Seu saldo: {saldo}</h3>
-      <input type="number" onChange={(e) => setCredito(e.target.value)} />
+
+      <input type="number" onChange={(e) => setCredito(e.target.value)} value={credito}/>
+
       <button onClick={() => depositarSaldo(credito)}>Depositar</button>
       <button onClick={() => sacarSaldo(credito)}>Sacar</button>
     </div>
--- Desafios-Context+Reducer/context-reducer/src/context/BankContex.jsx
+++ Desafios-Context+Reducer/context-reducer/src/context/BankContex.jsx
@@ -1,26 +1,33 @@
-import { createContext,  useReducer } from "react";
+import { createContext, useReducer } from "react";
 
-export const BankContext = createContext()
+export const BankContext = createContext();
 
-export const BankReducer = (state, action)=>{
+export const BankReducer = (state, action) => {
+  switch (action.type) {
+    case "DEPOSITAR":
+      return { ...state, saldo: state.saldo + Number(action.dados) };
 
-    switch(action.type){
+    case "SACAR":
+      const valorSaque = Number(action.dados); // convertemos o que vem do cliente para number e atribuimos a uma variavel para utilizar em outras coisas dentro do bloco
 
-        case 'DEPOSITAR':
-            return {...state, saldo: action.dados};
-        case 'SACAR':
-            return {...state, saldo: action.dados};
-        default:
-            return state;
-    }
-}
+      if (valorSaque > state.saldo) {
+        alert(`Valor indisponivel, você tem R$${state.saldo} na conta`);
+        return state
+      }
+      // toda lógica matematica deve ficar dentro do reducer
+      return { ...state, saldo: state.saldo - valorSaque }; 
 
-export const BankContextProvider = ({children}) =>{
-    const [state, dispatch] = useReducer(BankReducer, {saldo:'0'})
+    default:
+      return state;
+  }
+};
 
-    return(
-        <BankContext.Provider value={{...state, dispatch}}>
-            {children}
-        </BankContext.Provider>
-    )
-}
+export const BankContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(BankReducer, { saldo: 0 });
+
+  return (
+    <BankContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </BankContext.Provider>
+  );
+};
