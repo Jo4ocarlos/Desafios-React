d3395ff68909e57a2bcd585a03649e217853e0e5
João Carlos de Almeida <juke4892@gmail.com>
2026-05-04 16:23:52 -0300

    criação de alguns arquivos iniciais

--- Mini Blog/blog/src/App.jsx
+++ Mini Blog/blog/src/App.jsx
@@ -1,4 +1,6 @@
-
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
 import './App.css'
 
 function App() {
@@ -6,6 +8,12 @@ function App() {
   return (
     <>
      <h1>MiniBLog</h1>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' elemen={<Home/>}/>
+      <Route path='/about' elemen={<About/>}/>
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
---
+++ Mini Blog/blog/src/firebase/config.js
@@ -0,0 +1,22 @@
+// inicialização do firebase
+import { initializeApp } from "firebase/app";
+
+import {getFirestore} from 'firebase/firebase'
+// depois iniciamos o banco de dados da firestore
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuOWOouOCoXf4Ebsn0IAftOvUo9K3ZUeQ",
+  authDomain: "miniblog-fb604.firebaseapp.com",
+  projectId: "miniblog-fb604",
+  storageBucket: "miniblog-fb604.firebasestorage.app",
+  messagingSenderId: "372487316503",
+  appId: "1:372487316503:web:e4915bfaef3d0b18ffe852"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// banco de dados
+const db = getFirestore(app)
+
+export {db}; 
