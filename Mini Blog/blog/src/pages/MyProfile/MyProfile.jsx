bf57488aef69690a5ea52a1a5a117d0305b0f4bc
João Carlos de Almeida <juke4892@gmail.com>
2026-06-01 17:52:04 -0300

    conclusão do hook de exclusão do post da pagina do usuario

--- Mini Blog/blog/src/pages/MyProfile/MyProfile.jsx
+++ Mini Blog/blog/src/pages/MyProfile/MyProfile.jsx
@@ -1,22 +1,17 @@
 import style from "./MyProfile.module.css";
-import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { Link } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const MyProfile = () => {
   const { user } = useAuthValue();
-  const { documents: userPosts, loading } = useFetchDocuments(
-    "posts",
-    null,
-    user.uid,
-  );
+  const { documents: userPosts, loading } = useFetchDocuments("posts", null, user.uid);
+  
+  const { deleteDocument } = useDeleteDocument("posts");
 
   if (loading) return <p className={style.loading}>Carregando seus dados...</p>;
 
-  /*: 
-    Se userPosts for null ou undefined, nossa constante vira um array vazio [],
-    impedindo que o React quebre ao tentar ler o .length lá embaixo!
-  */
   const postsList = userPosts || [];
 
   return (
@@ -41,11 +36,9 @@ const MyProfile = () => {
       <div className={style.posts_section}>
         <div className={style.section_title_wrapper}>
           <h2>Minhas Publicações</h2>
-          {/* Usamos postsList com segurança aqui */}
           <span className={style.post_count}>{postsList.length} posts</span>
         </div>
 
-        {/* Verificação se está vazio usando a constante segura */}
         {postsList.length === 0 ? (
           <div className={style.noposts}>
             <p>Você ainda não fez nenhuma publicação.</p>
@@ -55,38 +48,38 @@ const MyProfile = () => {
           </div>
         ) : (
           <ul className={style.profile_feed}>
-            {/* Mapeamos a postsList com segurança aqui também */}
             {postsList.map((post) => (
               <li key={post.id} className={style.profile_post_card}>
                 <div className={style.card_header}>
                   <span className={style.post_date}>Postado recentemente</span>
                   <div className={style.card_actions}>
-                    <Link>Editar</Link>
+                    <Link to={`/posts/edit/${post.id}`}>Editar</Link>
                   </div>
                 </div>
 
                 <div className={style.card_body}>
-                  
-                    {post.urlImg && (
-                      <img
-                        src={post.urlImg}
-                        alt={`Imagem do post ${post.title}`}
-                        className={style.post_thumbnail}
-                      />
-                    )}
-                    <div className={style.card_infs}>
+                  {post.urlImg && (
+                    <img
+                      src={post.urlImg}
+                      alt={`Imagem do post ${post.title}`}
+                      className={style.post_thumbnail}
+                    />
+                  )}
+                  <div className={style.card_infs}>
                     <div>
                       <h3>{post.title}</h3>
                       <p>
-                        {post.content
-                          ? post.content.substring(0, 120) + "..."
-                          : ""}
+                        {post.content ? post.content.substring(0, 120) + "..." : ""}
                       </p>
                     </div>
                     <div>
-                      <button className={style.icon_btn}>Excluir</button>
+                      <button
+                        className={style.icon_btn}
+                        onClick={() => deleteDocument(post.id)}
+                      >
+                        Excluir
+                      </button>
                     </div>
-
                   </div>
                 </div>
               </li>
---
+++ Mini Blog/blog/src/pages/userAuthenticatedPages/EditPost/EditPost.jsx
@@ -0,0 +1,113 @@
+import styles from "./CreatePost.module.css";
+
+//hooks
+import { useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom'
+import { useAuthValue } from "../../../context/AuthContext";
+import { useInsertDocument } from "../../../hooks/useInsertDocument";
+
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [urlImg, setUrlImg] = useState("");
+  const [content, setContent] = useState("");
+  const [hashTags, setHashTags] = useState([""]);
+  const [formError, setFormError] = useState('')
+
+  const {insertDocument, response} = useInsertDocument('posts')
+  const {user} = useAuthValue()
+
+  const navigate = useNavigate()
+
+  const handleTags = (e)=>{
+    //convertemos o que vem do input como string para array, a cada virgula que o usuario digitar vira um componente do array
+    const stringToArray = e.target.value.split(',').map(tags => tags.trim()).filter(Boolean)
+    //tive que adicionar esse metodo filter(Boolean) porque quando adicionava a virgula criava uma tag vazia, apenas com espaço
+    setHashTags(stringToArray);
+  }
+  console.log(hashTags)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError('')
+
+    try{
+      new URL(urlImg)
+    } catch (error){
+      setFormError("Precisamos de uma url valida")
+      return
+    }
+
+    if(!title || !urlImg || !hashTags  || !content){
+      setFormError("Por favor, preencha todos os campos!")
+      return
+    }
+
+
+    const newPost = {
+      title,
+      urlImg,
+      content,
+      hashTags,
+      uid: user.uid,
+      createdBy: user.displayName
+    }
+
+    insertDocument(newPost)
+
+    console.log(newPost)
+    navigate('/')
+  };
+
+  useEffect(()=>{
+    setFormError(response.error)
+  },[response])
+  return (
+    <div className={styles.create_post_page}>
+      <h1>Crie sua postagem!</h1>
+      <p>Escreva sobre o que quiser e compartilhe suas ideias</p>
+      <form onSubmit={handleSubmit}>
+        
+        <label>
+          <span>Título do post</span>
+          <input
+            type="text"
+            name="title"
+            placeholder="pense em um bom título"
+          onChange={(e)=> setTitle(e.target.value)} required/>
+        </label>
+
+        <label>
+          <span>URL da imagem</span>
+          <input
+            type="url"
+            name="urlImg"
+            id=""
+            placeholder="insira uma imagem que represente seu post"
+          onChange={(e)=> setUrlImg(e.target.value)} required/>
+        </label>
+
+        <label>
+          <span>Conteúdo</span>
+          <textarea name="content" required onChange={(e)=> setContent(e.target.value) } placeholder="No que você está pensando?" value={content}></textarea>
+        </label>
+        
+        <label>
+          <span>HashTags:</span>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="insira hashtags separadas por vírgulas"
+            onChange={handleTags}
+            required
+          />
+        </label>
+        {response.loading && <button className="btn" disabled>Criando sua postagem...</button>}
+        {!response.loading && <button className="btn">Criar post!</button>}
+      </form>
+      {formError && <p className="error">{formError}</p>}
+    </div>
+  );
+};
+
+export default CreatePost;
